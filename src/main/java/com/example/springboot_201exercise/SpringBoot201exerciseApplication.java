3049b2a5d87fe55edb3fa05def8865e7eb8fefc7
RuthWudu <ruthwudu576@gmail.com>
2021-07-27 08:39:47 -0400

    form practice

---
+++ src/main/java/com/example/springboot_201exercise/SpringBoot201exerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_201exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot201exerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot201exerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_201exercise/SpringBoot201exerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_201exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot201exerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
